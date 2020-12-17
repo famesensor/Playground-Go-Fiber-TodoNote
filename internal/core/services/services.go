4329b0406737163332f26490cafbd00cbb1d023a
famesensor <famfew25@gmail.com>
2020-12-17 17:37:32 +0700

    :fire: remove file temp

--- internal/core/services/services.go
+++
@@ -1,9 +0,0 @@
-package ports
-
-import "github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain"
-
-type UserService interface {
-	Create(user *domain.Todo) error
-	FindById(id string) (*domain.Todo, error)
-	FindAll() (*[]domain.Todo, error)
-}
