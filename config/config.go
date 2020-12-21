2497aac620dac8a07e933e13404a2824c2f2a022
famesensor <famfew25@gmail.com>
2020-12-21 11:23:25 +0700

    [update]: add config mongoDB

--- config/config.go
+++ config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"os"
+	"time"
+)
+
+type config struct {
+	host     string
+	database string
+	port     string
+	driver   string
+	user     string
+	password string
+	timeout  time.Duration
+}
+
+// Config mongoDB
+func newConfigMongoDB() *config {
+	return &config{
+		host:     os.Getenv("MONGO_HOST"),
+		database: os.Getenv("MONGO_HOST"),
+		password: os.Getenv("MONGO_ROOT_PASSWORD"),
+		user:     os.Getenv("MONGO_ROOT_USER"),
+		timeout:  10 * time.Second,
+	}
+}
