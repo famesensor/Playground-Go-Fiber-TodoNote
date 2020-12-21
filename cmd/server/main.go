7082abe911d78c575efcc052570c79bacb459e4e
famesensor <famfew25@gmail.com>
2020-12-21 11:23:06 +0700

    [update]: init main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	// todoRepository := mongo.NewMongoRepositotry()
+	// todoService := todo.New()
+	// todoHandler := handler.New()
+
+	// fiber inti
+	app := fiber.New()
+
+	// server run
+	log.Fatal(app.Listen(":30000"))
+}
+
+func setupRotue() {
+
+}
