6dbb4b61075062dfebf941f325a22560b39ae729
famesensor <famfew25@gmail.com>
2020-12-21 11:25:05 +0700

    [update]: add service create and find todo

--- internal/core/services/todo/services.go
+++ internal/core/services/todo/services.go
@@ -1,1 +1,40 @@
 package todo
+
+import (
+	"fmt"
+
+	"github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain"
+	"github.com/famesensor/playground-go-fiber-todonotes/internal/core/ports"
+)
+
+type service struct {
+	todoRepository ports.TodoRepository
+}
+
+func New(todoRepository ports.TodoRepository) *service {
+	return &service{
+		todoRepository,
+	}
+}
+
+func (srv *service) CreateTodo(todo *domain.Todo) error {
+	fmt.Print(todo)
+
+	err := srv.todoRepository.Create(todo)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (srv *service) FindTodoId(id string) (*domain.Todo, error) {
+	fmt.Print(id)
+
+	res, err := srv.FindTodoId(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
