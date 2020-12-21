f4e10efcffbcd84f9d8e6a8c63fbd4a23494b611
famesensor <famfew25@gmail.com>
2020-12-21 11:24:38 +0700

    [update]: remove interface

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -5,5 +5,4 @@ import "github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain
 type TodoService interface {
 	Create(user *domain.Todo) error
 	FindById(id string) (*domain.Todo, error)
-	FindAll() (*[]domain.Todo, error)
 }
