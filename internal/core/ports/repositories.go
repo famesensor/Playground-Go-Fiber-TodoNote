19fe66bfe9a7352b6b9199397ebb969e78a2c359
famesensor <famfew25@gmail.com>
2020-12-24 23:00:36 +0700

    [update]: fix error return value

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -5,5 +5,5 @@ import "github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain
 type TodoRepository interface {
 	Create(user *domain.Todo) error
 	FindById(id string) (*domain.Todo, error)
-	FindAll() (*[]domain.Todo, error)
+	FindAll() ([]*domain.Todo, error)
 }
