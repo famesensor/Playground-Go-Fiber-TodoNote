f0e715d048c573148878107cef1e282d3f6c203a
famesensor <famfew25@gmail.com>
2020-12-24 23:02:20 +0700

    [update]: add handler interface and add findall handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,44 +6,54 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type TodoHandler interface {
+	Create(c *fiber.Ctx) error
+	FindById(c *fiber.Ctx) error
+	FindAll(c *fiber.Ctx) error
+}
+
 type todoHandler struct {
 	todoService ports.TodoService
 }
 
-func New(todoService ports.TodoService) *todoHandler {
+func New(todoService ports.TodoService) TodoHandler {
 	return &todoHandler{
 		todoService,
 	}
 }
 
-func (h *todoHandler) CreateTodo(c *fiber.Ctx) {
+func (h *todoHandler) Create(c *fiber.Ctx) error {
 	var todo domain.Todo
 
 	if err := c.BodyParser(todo); err != nil {
-		return
+		return c.JSON("Error na ka")
 	}
 
 	if err := h.todoService.Create(&todo); err != nil {
-		return
+		return c.JSON("Error na ka")
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"status": true,
 		"data":   "success",
 	})
 
 }
 
-func (h *todoHandler) FindTodoId(c *fiber.Ctx) {
+func (h *todoHandler) FindById(c *fiber.Ctx) error {
 	id := c.Params("id")
 
 	res, err := h.todoService.FindById(id)
 	if err != nil {
-		return
+		return c.JSON("Error na ka")
 	}
 
-	c.JSON(fiber.Map{
+	return c.JSON(fiber.Map{
 		"Status": true,
 		"data":   res,
 	})
+}
+
+func (h *todoHandler) FindAll(c *fiber.Ctx) error {
+	return c.JSON("Error na ka")
 }
