c7c4ea91a09ba527b7888624ca95826f67cd6060
famesensor <famfew25@gmail.com>
2020-12-24 23:00:56 +0700

    [update]: add findall todo

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -5,4 +5,5 @@ import "github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain
 type TodoService interface {
 	Create(user *domain.Todo) error
 	FindById(id string) (*domain.Todo, error)
+	FindAll() ([]*domain.Todo, error)
 }
