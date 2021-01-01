9a45635da9c542f7b9ada47e1f64083ebc883e25
famesensor <famfew25@gmail.com>
2021-01-02 01:03:14 +0700

    [update]: addd id and set bson and jsonin struct

--- internal/core/domain/domain.go
+++ internal/core/domain/domain.go
@@ -1,6 +1,14 @@
 package domain
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Todo struct {
-	Content   string `json:content`
-	CreatedAt int    `json:createdAt`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Content   string             `json:"content,omitempty" bson:"content,omitempty"`
+	CreatedAt time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
