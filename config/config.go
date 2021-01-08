9b344a28281ba24cd5da5a73542775b78b428efb
famesensor <famfew25@gmail.com>
2021-01-09 00:27:39 +0700

    [update]: chage get env

--- config/config.go
+++ config/config.go
@@ -1,27 +1,41 @@
 package config
 
 import (
+	"fmt"
+	"log"
 	"os"
-	"time"
+
+	"github.com/Netflix/go-env"
+	"github.com/joho/godotenv"
 )
 
-type config struct {
-	host     string
-	database string
-	port     string
-	driver   string
-	user     string
-	password string
-	timeout  time.Duration
+type Config struct {
+	Env           string `env:"APP_ENV"`
+	ServerPort    string `env:"APP_SERVER_PORT"`
+	MongoHost     string `env:"MONGO_HOST"`
+	MongoDatabase string `env:"MONGO_DATABASE"`
+	MongoPort     string `env:"MONGO_PORT"`
+	MongoUser     string `env:"MONGO_USER"`
+	MongoPassword string `env:"MONGO_PASSWORD"`
 }
 
-// Config mongoDB
-func newConfigMongoDB() *config {
-	return &config{
-		host:     os.Getenv("MONGO_HOST"),
-		database: os.Getenv("MONGO_HOST"),
-		password: os.Getenv("MONGO_ROOT_PASSWORD"),
-		user:     os.Getenv("MONGO_ROOT_USER"),
-		timeout:  10 * time.Second,
+func ParseConfig() (cfg *Config) {
+	godotenv.Load(".env")
+	switch os.Getenv("APP_ENV") {
+	case "local":
+		godotenv.Overload(".env")
+		log.Println("Running on local environments")
+		break
+	default:
+		log.Println("Running on production environments")
+		break
 	}
+
+	cfg = new(Config)
+	_, err := env.UnmarshalFromEnviron(cfg)
+	if err != nil {
+		fmt.Printf("Cannot read config %s\n", err.Error())
+		os.Exit(1)
+	}
+	return
 }
