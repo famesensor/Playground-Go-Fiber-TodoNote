f2c7c3e5978f4dd33e1af974153c0b9ab156b929
famesensor <famfew25@gmail.com>
2021-01-09 15:42:13 +0700

    [update]: update all function

--- internal/handler/http-todo.go
+++ internal/handler/http-todo.go
@@ -3,6 +3,7 @@ package handler
 import (
 	model "github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain"
 	interfaces "github.com/famesensor/playground-go-fiber-todonotes/internal/core/ports"
+	"github.com/famesensor/playground-go-fiber-todonotes/pkg/errs"
 	"github.com/famesensor/playground-go-fiber-todonotes/pkg/reponseHandler"
 	"github.com/gofiber/fiber/v2"
 )
@@ -21,35 +22,64 @@ func (h *todoHandler) Create(c *fiber.Ctx) error {
 	todo := new(model.Todo)
 
 	if err := c.BodyParser(&todo); err != nil {
-		return c.JSON("Error na ka")
+		return errs.ErrorReponse(c, errs.CannotParseData, nil)
 	}
 
+	// TODO: Validate data
+
 	if err := h.todoService.Create(c.Context(), todo); err != nil {
-		return c.JSON("Error na ka")
+		return errs.ErrorReponse(c, err, nil)
 	}
 
 	return reponseHandler.ReponseMsg(c, 201, "success", "", nil)
 }
 
 func (h *todoHandler) FindById(c *fiber.Ctx) error {
-	id := c.Params("id")
+	id := c.Query("todoId")
+
+	// TODO: Validate ID
 
 	res, err := h.todoService.FindById(c.Context(), id)
 	if err != nil {
-		return c.JSON("Error na ka")
+		return errs.ErrorReponse(c, err, nil)
 	}
 
 	return reponseHandler.ReponseMsg(c, 200, "success", "", res)
 }
 
 func (h *todoHandler) FindAll(c *fiber.Ctx) error {
-	return c.JSON("Error na ka")
+	todos, err := h.todoService.FindAll(c.Context())
+	if err != nil {
+		return errs.ErrorReponse(c, err, nil)
+	}
+	return reponseHandler.ReponseMsg(c, 200, "success", "", todos)
 }
 
-func (h *todoHandler) Update(c *fiber.Ctx) error {
-	return nil
+func (h *todoHandler) UpdateTodo(c *fiber.Ctx) error {
+	todo := new(model.Todo)
+	id := c.Query("id")
+
+	if err := c.BodyParser(&todo); err != nil {
+		return errs.ErrorReponse(c, errs.CannotParseData, nil)
+	}
+
+	// TODO: Validate Date
+
+	if err := h.todoService.UpdateTodo(c.Context(), id, todo); err != nil {
+		return errs.ErrorReponse(c, err, nil)
+	}
+
+	return reponseHandler.ReponseMsg(c, 200, "success", "", nil)
 }
 
-func (h *todoHandler) Delete(c *fiber.Ctx) error {
-	return nil
+func (h *todoHandler) UpdateStatus(c *fiber.Ctx) error {
+	id := c.Query("id")
+
+	// TODO: Validate ID
+
+	if err := h.todoService.UpdateStatus(c.Context(), id); err != nil {
+		return errs.ErrorReponse(c, err, nil)
+	}
+
+	return reponseHandler.ReponseMsg(c, 200, "success", "", nil)
 }
