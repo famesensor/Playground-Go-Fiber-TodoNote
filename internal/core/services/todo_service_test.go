665fbe202372ece1a5e11c63e01bd9fc21fd5727
famesensor <sathit.ju@outlook.com>
2021-04-15 15:39:20 +0700

    [:hammer:]: update test script

--- internal/core/services/todo_service_test.go
+++ internal/core/services/todo_service_test.go
@@ -2,14 +2,29 @@ package services_test
 
 import (
 	"context"
+	"errors"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/core/domain"
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/core/services"
-	_mock "github.com/famesensor/playground-go-fiber-todonotes/internal/mocks"
+	"github.com/famesensor/playground-go-fiber-todonotes/internal/mocks"
+	"github.com/stretchr/testify/mock"
 	"github.com/tj/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+var mockTodoRepo *mocks.TodoRepository
+var ctx context.Context
+var unexpectedError = errors.New("Unexpected Error")
+
+func TestMain(m *testing.M) {
+	mockTodoRepo = new(mocks.TodoRepository)
+	ctx = context.Background()
+	code := m.Run()
+	os.Exit(code)
+}
 
 func TestCreate(t *testing.T) {
 	mockRequest := &domain.Todo{
@@ -17,47 +32,153 @@ func TestCreate(t *testing.T) {
 		Description: "Unit Testing",
 		Status:      "Waiting",
 		Priority:    1,
-		CreatedAt:   time.Date(2021, 03, 01, 12, 00, 00, 00, time.Local),
+		CreatedAt:   time.Now(),
 	}
 
 	t.Run("success", func(t *testing.T) {
-		mockTodoRepo := new(_mock.TodoRepository)
-
-		mockTodoRepo.On("Create", context.TODO(), mockRequest).Return(nil).Once()
-
-		u := services.NewTodoService(mockTodoRepo)
-
-		err := u.Create(context.TODO(), mockRequest)
-
+		mockTodoRepo.On("Create", context.TODO(), mock.AnythingOfType("*domain.Todo")).Return(nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.Create(ctx, mockRequest)
 		assert.NoError(t, err)
-		// assert.Equal(t)
 		mockTodoRepo.AssertExpectations(t)
 	})
 
 	t.Run("error", func(t *testing.T) {
-		mockTodoRepo := new(_mock.TodoRepository)
-		u := services.NewTodoService(mockTodoRepo)
-
-		err := u.Create(context.TODO(), mockRequest)
-
+		mockTodoRepo.On("Create", context.TODO(), mock.AnythingOfType("*domain.Todo")).Return(unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.Create(ctx, mockRequest)
 		assert.Error(t, err)
-		// assert.Equal(t)
 		mockTodoRepo.AssertExpectations(t)
 	})
 }
 
 func TestFindById(t *testing.T) {
+	mockTodoId := "todo_uuid_id"
+	mockTodoRes := &domain.Todo{
+		ID:          primitive.NewObjectID(),
+		Title:       "title todo",
+		Description: "description todo",
+		Status:      "success",
+		Priority:    1,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	t.Run("success", func(t *testing.T) {
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		_, err := s.FindById(ctx, mockTodoId)
+		assert.NoError(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 
+	t.Run("error", func(t *testing.T) {
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		_, err := s.FindById(ctx, mockTodoId)
+		assert.Error(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 }
 
 func TestFindAll(t *testing.T) {
+	mockTodoRes := []*domain.Todo{
+		{
+			ID:          primitive.NewObjectID(),
+			Title:       "title todo",
+			Description: "description todo",
+			Status:      "success",
+			Priority:    1,
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		},
+	}
 
+	t.Run("success", func(t *testing.T) {
+		mockTodoRepo.On("FindAll", context.TODO()).Return(mockTodoRes, nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		_, err := s.FindAll(ctx)
+		assert.NoError(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		mockTodoRepo.On("FindAll", context.TODO()).Return(mockTodoRes, unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		_, err := s.FindAll(ctx)
+		assert.Error(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 }
 
 func TestUpdateTodo(t *testing.T) {
+	mockTodoId := "todo_uuid_id"
+	mockTodoRes := &domain.Todo{
+		Title:       "title todo",
+		Description: "description todo",
+		Status:      "success",
+		Priority:    1,
+	}
 
+	t.Run("success", func(t *testing.T) {
+		mockTodoRepo.On("UpdateTodo", context.TODO(), mock.AnythingOfType("string"), mock.AnythingOfType("*domain.Todo")).Return(nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateTodo(ctx, mockTodoId, mockTodoRes)
+		assert.NoError(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		mockTodoRepo.On("UpdateTodo", context.TODO(), mock.AnythingOfType("string"), mock.AnythingOfType("*domain.Todo")).Return(unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateTodo(ctx, mockTodoId, mockTodoRes)
+		assert.Error(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 }
 
 func TestUpdateStatus(t *testing.T) {
+	mockTodoId := "todo_uuid_id"
+	mockTodoRes := &domain.Todo{
+		ID:          primitive.NewObjectID(),
+		Title:       "title todo",
+		Description: "description todo",
+		Status:      "in progress",
+		Priority:    1,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	t.Run("success", func(t *testing.T) {
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, nil).Once()
+		mockTodoRepo.On("UpdateStatus", context.TODO(), mock.AnythingOfType("string")).Return(nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateStatus(ctx, mockTodoId)
+		assert.NoError(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 
+	t.Run("success", func(t *testing.T) {
+		mockTodoRes.Status = "success"
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, nil).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateStatus(ctx, mockTodoId)
+		assert.NoError(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateStatus(ctx, mockTodoId)
+		assert.Error(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
+	t.Run("error", func(t *testing.T) {
+		mockTodoRes.Status = "in progress"
+		mockTodoRepo.On("FindById", context.TODO(), mock.AnythingOfType("string")).Return(mockTodoRes, nil).Once()
+		mockTodoRepo.On("UpdateStatus", context.TODO(), mock.AnythingOfType("string")).Return(unexpectedError).Once()
+		s := services.NewTodoService(mockTodoRepo)
+		err := s.UpdateStatus(ctx, mockTodoId)
+		assert.Error(t, err)
+		mockTodoRepo.AssertExpectations(t)
+	})
 }
