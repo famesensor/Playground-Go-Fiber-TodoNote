aab2d72a19c9fbd8d9647a582e2f87e901adf371
famesensor <sathit.ju@outlook.com>
2021-04-15 15:40:03 +0700

    :memo: add swagger docs

--- internal/core/ports/handler.go
+++ internal/core/ports/handler.go
@@ -3,9 +3,66 @@ package ports
 import "github.com/gofiber/fiber/v2"
 
 type TodoHandler interface {
+	// // Create create a new book data
+	// // @Summary Create a new todo
+	// // @Description Create book
+	// // @Tags todo
+	// // @Accept json
+	// // @Produce json
+	// // @Param todo body model.Todo true "Create book"
+	// // @Success 200 {object} ResponseHTTP{}
+	// // @Failure 400 {object} ResponseHTTP{}
+	// // @Router  /api/v1/create [post]
 	Create(c *fiber.Ctx) error
+
+	// // FindById is a function to get a todo by ID
+	// // @Summary Get todo by ID
+	// // @Description Get todo by ID
+	// // @Tags todos
+	// // @Accept json
+	// // @Produce json
+	// // @Param id path int true "TODO ID"
+	// // @Success 200 {object} ResponseHTTP{data=[]model.Todo}
+	// // @Failure 404 {object} ResponseHTTP{}
+	// // @Failure 500 {object} ResponseHTTP{}
+	// // @Router /api/v1/:id [get]
 	FindById(c *fiber.Ctx) error
+
+	// FindAll is a function to getall Todo data from database
+	// @Summary Get all Todos
+	// @Description Get all todos
+	// @Tags todos
+	// @Accept json
+	// @Produce json
+	// @Success 200 {object} ResponseHTTP{data=[]model.Todo}
+	// @Failure 404 {object} ResponseHTTP{}
+	// @Failure 500 {object} ResponseHTTP{}
+	// @Router /todo/todos [get]
 	FindAll(c *fiber.Ctx) error
+
+	// // UpdateTodo function removes a book by ID
+	// // @Summary Remove book by ID
+	// // @Description Remove book by ID
+	// // @Tags books
+	// // @Accept json
+	// // @Produce json
+	// // @Param id path int true "Book ID"
+	// // @Success 200 {object} ResponseHTTP{}
+	// // @Failure 404 {object} ResponseHTTP{}
+	// // @Failure 503 {object} ResponseHTTP{}
+	// // @Router /v1/books/{id} [delete]
 	UpdateTodo(c *fiber.Ctx) error
+
+	// // DeleteBook function removes a book by ID
+	// // @Summary Remove book by ID
+	// // @Description Remove book by ID
+	// // @Tags books
+	// // @Accept json
+	// // @Produce json
+	// // @Param id path int true "Book ID"
+	// // @Success 200 {object} ResponseHTTP{}
+	// // @Failure 404 {object} ResponseHTTP{}
+	// // @Failure 503 {object} ResponseHTTP{}
+	// // @Router /v1/books/{id} [delete]
 	UpdateStatus(c *fiber.Ctx) error
 }
--- protocol/http.go
+++ protocol/http.go
@@ -4,18 +4,29 @@ import (
 	"context"
 	"log"
 
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/famesensor/playground-go-fiber-todonotes/config"
+	_ "github.com/famesensor/playground-go-fiber-todonotes/docs"
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/actors/database"
 	interfaces "github.com/famesensor/playground-go-fiber-todonotes/internal/core/ports"
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/core/services"
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/handler"
 	"github.com/famesensor/playground-go-fiber-todonotes/internal/repositories"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:3000
+// @BasePath /api/v1
 func ServerHttp() error {
 	app := fiber.New()
 
@@ -53,13 +64,14 @@ func ServerHttp() error {
 }
 
 func setupRoute(app *fiber.App, todoHandler interfaces.TodoHandler) {
+	app.Get("/docs/*", swagger.Handler) // default
 	v1 := app.Group("/api/v1", logger.New())
 
 	// Todo route
 	todo := v1.Group("/todo")
+	todo.Get("/todos", todoHandler.FindAll)
 	todo.Post("/create-todo", todoHandler.Create)
 	todo.Patch("/update-todo", todoHandler.UpdateTodo)
 	todo.Patch("/update-status", todoHandler.UpdateStatus)
-	todo.Get("/todos", todoHandler.FindAll)
-	todo.Get("/", todoHandler.FindById)
+	todo.Get("/:id", todoHandler.FindById)
 }
